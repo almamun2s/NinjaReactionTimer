ed6b7715521328a74c6ece8b5ffca88c22c91f59
Abdullah Almamun <mamun2social@gmail.com>
2024-12-09 12:09:13 +0600

    ninja reaction timer created

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Ninja reaction timer</h1>
+  <button @click="play" :disabled="isPlaying">Start</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Result v-if="showScore" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Block, Result },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showScore: false,
+    };
+  },
+  methods: {
+    play() {
+      this.delay = 1000 + Math.random() * 3000;
+      this.isPlaying = true;
+      this.showScore = false;
+    },
+    endGame(reactionTime) {
+      this.isPlaying = false;
+      this.score = reactionTime;
+      this.showScore = true;
+    },
+  },
+};
 </script>
 
 <style>
@@ -20,7 +41,21 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+button {
+  background-color: #0faf87;
+  color: #fff;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 5px;
+  font-size: 1rem;
+  margin: 10px;
+  cursor: pointer;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,40 @@
+<template>
+  <p>Reaction Time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 200) {
+      this.rank = "Wow! Ninja Fingers!";
+    } else if (this.score < 300) {
+      this.rank = "Rapid Reflexes";
+    } else if (this.score < 500) {
+      this.rank = "Not Bad";
+    } else if (this.score < 700) {
+      this.rank = "Not Good";
+    } else if (this.score < 1000) {
+      this.rank = "So Slow...";
+    } else if (this.score < 1500) {
+      this.rank = "Better next time...";
+    } else {
+      this.rank = "Snail Pace...";
+    }
+  },
+};
+</script>
+
+<style scoped>
+.rank {
+  font-size: 1.4rem;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
